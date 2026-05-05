be1cf7c53e2b972dd10b7c823cba95c632806552
dejavukatherine-byte <dejavu.katherine@gmail.com>
2026-05-05 17:01:14 +0800

    Update HomePage.tsx

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router';
 //import silverRingImage from 'figma:asset/f5e4b07b6669741a2be6bdc3426ad2b6.jpg';
-import necklaceImage from 'figma:asset/8d736274f039993717a5886b45062b71.jpg';
+//import necklaceImage from 'figma:asset/8d736274f039993717a5886b45062b71.jpg';
 import processImage from 'figma:asset/image-1.png';
 import processImage2 from 'figma:asset/微信图片_20260504162238_431_2-2.png';
 import banner1 from 'figma:asset/微信图片_20260402132534_367_740-1.png';
@@ -116,7 +116,7 @@ export default function HomePage() {
             </div>
             <div className="relative h-80 sm:h-96 rounded-lg overflow-hidden group cursor-pointer">
               <img
-                src={necklaceImage}
+               src="[https://via.placeholder.com/800x800](https://via.placeholder.com/800x800)"
                 alt="Brass Jewelry"
                 className="w-full h-full object-cover"
                 loading="lazy"
